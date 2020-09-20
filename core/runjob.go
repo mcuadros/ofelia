41406b487d70cb250c75e7750d9af3a9b49bbbcf
Taras <9948629+Trane9991@users.noreply.github.com>
2020-09-20 14:06:35 +0300

    read logs of container created via job-run (#115)
    read logs of container created via job-run
    
    fixes #103

--- core/runjob.go
+++ core/runjob.go
@@ -86,6 +86,17 @@ func (j *RunJob) Run(ctx *Context) error {
 		return err
 	}
 
+	if err := j.Client.Logs(docker.LogsOptions{
+		Container:    container.ID,
+		OutputStream: ctx.Execution.OutputStream,
+		ErrorStream:  ctx.Execution.ErrorStream,
+		Stdout:       true,
+		Stderr:       true,
+		RawTerminal:  true,
+	}); err != nil {
+		return err
+	}
+
 	if j.Container == "" {
 		return j.deleteContainer(container.ID)
 	}
