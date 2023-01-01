876a7153adbc36a854339913186590042889537c
Taras <9948629+Trane9991@users.noreply.github.com>
2023-01-01 15:59:09 +0000

    Fix flaky test
    The test expects duration in seconds to be more than 0,
    but there is no reason to use `Seconds()`

--- core/common_test.go
+++ core/common_test.go
@@ -215,7 +215,7 @@ func (s *SuiteCommon) TestExecutionStopError(c *C) {
 	c.Assert(exe.Failed, Equals, true)
 	c.Assert(exe.Skipped, Equals, false)
 	c.Assert(exe.Error, Equals, err)
-	c.Assert(exe.Duration.Seconds() > .0, Equals, true)
+	c.Assert(exe.Duration > 0, Equals, true)
 }
 
 func (s *SuiteCommon) TestExecutionStopErrorSkip(c *C) {
@@ -227,7 +227,7 @@ func (s *SuiteCommon) TestExecutionStopErrorSkip(c *C) {
 	c.Assert(exe.Failed, Equals, false)
 	c.Assert(exe.Skipped, Equals, true)
 	c.Assert(exe.Error, Equals, nil)
-	c.Assert(exe.Duration.Seconds() > .0, Equals, true)
+	c.Assert(exe.Duration > 0, Equals, true)
 }
 
 func (s *SuiteCommon) TestMiddlewareContainerUseTwice(c *C) {
