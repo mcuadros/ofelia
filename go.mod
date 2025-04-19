a9f1b1121f29ae422fcaadf0bd1322b3b9de9762
taraspos <trane9991@gmail.com>
2025-04-19 14:05:20 +0100

    Update tests

--- go.mod
+++ go.mod
@@ -22,8 +22,7 @@ require (
 )
 
 require (
-	github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6 // indirect
-	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
@@ -39,16 +38,14 @@ require (
 	github.com/moby/sys/sequential v0.6.0 // indirect
 	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
-	github.com/moby/term v0.5.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.1.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.31.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gotest.tools/v3 v3.5.2 // indirect
 )
---
+++ integration/exec_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/magefile/mage/sh"
+)
+
+func TestExec(t *testing.T) {
+	projectName := strings.ToLower(t.Name())
+	outputDir := t.TempDir()
+	local_exec_filename := "local_exec.txt"
+	exec_filename := "exec.txt"
+	sleep_for_seconds := 15
+	schedule_seconds := 3
+
+	t.Setenv("COMPOSE_FILE", "./test-exec/docker-compose.yml")
+	t.Setenv("COMPOSE_PROJECT_NAME", projectName)
+	t.Setenv("OUTPUT_DIR", outputDir)
+	t.Setenv("SCHEDULE", fmt.Sprintf("@every %ds", schedule_seconds))
+	t.Setenv("SLEEP_SEC", strconv.Itoa(sleep_for_seconds))
+	t.Setenv("LOCAL_EXEC_OUTPUT_FILE", local_exec_filename)
+	t.Setenv("EXEC_OUTPUT_FILE", exec_filename)
+
+	t.Log("Printing docker compose config")
+	if err := sh.RunV("docker", "compose", "config"); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Running docker compose up")
+	if err := sh.RunV("docker", "compose", "up", "--exit-code-from", "sleep1"); err != nil {
+		t.Error(err)
+	}
+
+	for _, file := range []string{local_exec_filename, exec_filename} {
+		t.Log("Checking for outputs in", file)
+		data, err := os.ReadFile(filepath.Join(outputDir, file))
+		if err != nil {
+			t.Error(err)
+		}
+
+		split := strings.Split(string(data), "\n")
+		if expectedLines := sleep_for_seconds / schedule_seconds; len(split) != expectedLines {
+			t.Errorf("expected %d lines in %s, but got %d", expectedLines, file, len(split))
+		}
+	}
+}
